575d0d726d9a61ab8de4f328b547dff3e3016937
Dri912 <adrianaporrata0912@outlook.com>
2024-05-02 19:05:57 -0900

    bbbbb

--- src/util/External_Memory_Tree_Convert.java
+++ src/util/External_Memory_Tree_Convert.java
@@ -12,44 +12,39 @@ import java.util.LinkedList;
 
 public class External_Memory_Tree_Convert {
 
-    // Este metodo convierte el arbol en un arrayList con la informacion necesaria
-    // para ser serializado
+    // Este metodo convierte el arbol en un arrayList 
     public static ArrayList<Node_External> convert_Tree_to_ArrayList(HuffmanTree tree) {
 
-        // Se recorre el arbol en entreOrden y se almacenan sus nodos en una lista, y su
-        // posicion en una hastTable
-        PreOrderIterator it = tree.preOrderIterator();
-        LinkedList<BinaryTreeNode> list = new LinkedList<>();
-        HashMap<BinaryTreeNode, Integer> map = new HashMap<>();
+        PreOrderIterator ite = tree.preOrderIterator(); //Iterador en preorden para recorrer
+        LinkedList<BinaryTreeNode> list = new LinkedList<>(); //lista para almacenar sus nodos
+        HashMap<BinaryTreeNode, Integer> map = new HashMap<>(); //Hashmap para almacenar sus posiciones
 
-        int i = 0;
-        while (it.hasNext()) {
-            BinaryTreeNode node = it.nextNode();
-            list.add(node);
-            map.put(node, i++);
+        int i = 0; 
+        while (ite.hasNext()) {
+            BinaryTreeNode temp = ite.nextNode(); //Me quedo con el nodo 
+            list.add(temp); //lo agrego a la lista
+            map.put(temp, i++);  //guardo su posicion
         }
 
-        // Ahora creo un arrayList donde guardo la informacion de cada nodo, asi como si
-        // es terminal, y en que
-        // posicion esta su nodo derecho o -1 si no tiene
+        Iterator<BinaryTreeNode> iteList = list.iterator(); //Iterador para recorrer la lista
+        ArrayList<Node_External> aux = new ArrayList<>(list.size()); //lista para guardar la info de los nodos
 
-        Iterator<BinaryTreeNode> itList = list.iterator();
-        ArrayList<Node_External> outList = new ArrayList<>(list.size());
         i = 0;
-        while (itList.hasNext()) {
-            BinaryTreeNode node = itList.next();
-            Node_External node_external = new Node_External(node.getInfo());
 
-            boolean isterminal = node.getLeft() == null;
-            int right = node.getRight() == null ? -1 : map.get(node.getRight());
+        while (iteList.hasNext()) {
+            BinaryTreeNode node = iteList.next(); //guardo el nodo
+            Node_External temp = new Node_External(node.getInfo()); //la info
 
-            node_external.setTerminal(isterminal);
-            node_external.setRightNode(right);
+            boolean terminal = (node.getLeft() == null); 
+            int right = node.getRight() == null ? -1 : map.get(node.getRight()); //si es igual a null -1 sino su posicion
 
-            outList.add(i++, node_external);
+            temp.setTerminal(terminal); //guardo si es terminal
+            temp.setRightNode(right); //guardo la posicion de su hijo derecho
+
+            aux.add(i++, temp); //guardo en la posicion especificada el nodo
 
         }
-        return outList;
+        return aux; //retorno la lista
     }
 
     // Este metodo convierte el arraylist serializable en un arbol
